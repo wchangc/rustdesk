c705c542bee4e1b311524898a0fd860c4fac68a6
wchangc <48704286+wchangc@users.noreply.github.com>
2024-11-08 12:00:25 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.wchangc.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "aabbccdd";
+pub const PUBLIC_RS_PUB_KEY: &str = "cmQud2NoYW5nYy5jb20=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
